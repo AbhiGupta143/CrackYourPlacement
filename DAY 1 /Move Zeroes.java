c1fa3c669579330c6da1e5fa6c2b0abdbf93f57a
Abhishek Gupta <101378001+AbhiGupta143@users.noreply.github.com>
2024-07-15 19:32:28 +0530

    Move Zeroes.java

--- DAY 1 /Move Zeroes.java
+++ DAY 1 /Move Zeroes.java
@@ -1,1 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // Start with the first position
+        int insertPosition = 0;
 
+        for (int i=0; i< nums.length; i++){
+            // Fill all non-zero numbers
+            if(nums[i] !=0){
+                nums[insertPosition] = nums[i];
+                insertPosition++;
+            }
+        }
+        while (insertPosition < nums.length){
+            nums[insertPosition++] = 0;
+        }
+    }
+}
