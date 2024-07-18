4dedd7a0c5bdfa5ea222e4483ee050ed83ab73b2
Abhishek Gupta <101378001+AbhiGupta143@users.noreply.github.com>
2024-07-18 12:51:57 +0530

    Update Sort Colors.java

--- DAY 3 /Sort Colors.java
+++ DAY 3 /Sort Colors.java
@@ -1,1 +1,26 @@
-
+class Solution {
+    public void swap(int nums[], int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void sortColors(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        int index=0;
+        while(index<=end){
+            if(nums[index]==0){
+                swap(nums,index,start);
+                index++;
+                start++;
+            }
+            else if(nums[index] == 2){
+                swap(nums,index,end);
+                end--;
+            }
+            else{
+                index++;
+            }
+        }
+    }
+}
